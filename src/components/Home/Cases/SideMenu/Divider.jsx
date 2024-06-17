d05f4a90b65e9b4df041d1f4c08354b29cee18b4
Matrasos <sh.zhenis@yandex.kz>
2024-06-17 13:43:54 +0500

    checkpoint

---
+++ src/components/Home/Cases/SideMenu/Divider.jsx
@@ -0,0 +1,7 @@
+const Divider = () => {
+  return (
+    <div className="side-menu__divider"></div>
+  );
+}
+
+export default Divider;
--- src/components/Home/Cases/SideMenu/SideMenu.jsx
+++ src/components/Home/Cases/SideMenu/SideMenu.jsx
@@ -1,39 +1,84 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import Divider from './Divider';
 
 const SideMenu = ({ selectedCase, onClose }) => {
+  const [isVisible, setIsVisible] = useState(false);
+
   useEffect(() => {
-    document.body.classList.add('lock');
+    if (selectedCase) {
+      document.body.classList.add('lock');
+      setTimeout(() => setIsVisible(true), 10); // небольшая задержка для проигрывания анимации
+    } else {
+      document.body.classList.remove('lock');
+      setIsVisible(false);
+    }
+
     return () => {
       document.body.classList.remove('lock');
     };
-  }, []);
+  }, [selectedCase]);
 
   return (
-    <div className="side-menu">
-      <button onClick={onClose}>Close</button>
-      <h2>{selectedCase.title}</h2>
-      {selectedCase.src && <img src={selectedCase.src} alt={selectedCase.title} />}
-      <div className="side-menu-chips">
-        {selectedCase.chips.map((chip, index) => (
-          <div key={index}>{chip}</div>
-        ))}
+    <>
+      {isVisible && (
+        <div className="side-menu-overlay"></div>
+      )}
+      <div className={`side-menu ${isVisible ? 'open' : ''} ${selectedCase ? 'visible' : ''}`}>
+        <button onClick={onClose}>
+          <svg width="40" height="40" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <rect x="0.5" y="0.5" width="39" height="39" rx="19.5" stroke="black" />
+            <path d="M30 10L10 30M10 10L30 30" stroke="black" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+          </svg>
+        </button>
+
+        <h2 className='side-menu-subtitle'>Кейсы</h2>
+
+        {selectedCase && (
+          <>
+            <h2 className='side-menu-title'>{selectedCase.title}</h2>
+
+            <div className="side-menu-chips">
+              {selectedCase.chips.map((chip, index) => (
+                <div key={index}>{chip}</div>
+              ))}
+            </div>
+
+            <div className='side-menu-description' dangerouslySetInnerHTML={{ __html: selectedCase.description }}></div>
+
+            <Divider />
+
+            <h3 className='side-menu-title__common'>Задачи</h3>
+
+            {selectedCase.tasks.map((task, index) => (
+              <p className='side-menu-tasks-text' key={index}>{task}</p>
+            ))}
+
+            <Divider />
+
+            <h3 className='side-menu-title__common'>Решения</h3>
+
+            <ul className='side-menu-solution'>
+              {selectedCase.solutions.map((solution, index) => (
+                <li key={index}>{solution}</li>
+              ))}
+            </ul>
+
+            <Divider />
+
+            <h3 className='side-menu-title__common'>Итог</h3>
+
+            <ul className='side-menu-result'>
+              {selectedCase.results.map((result, index) => (
+                <li key={index}>{result}</li>
+              ))}
+            </ul>
+
+            <div className="side-menu-data">{selectedCase.data}</div>
+
+          </>
+        )}
       </div>
-      <div dangerouslySetInnerHTML={{ __html: selectedCase.description }}></div>
-      <h3>Tasks</h3>
-      <p>{selectedCase.tasks}</p>
-      <h3>Solutions</h3>
-      <ul>
-        {selectedCase.solutions.map((solution, index) => (
-          <li key={index}>{solution}</li>
-        ))}
-      </ul>
-      <h3>Results</h3>
-      <ul>
-        {selectedCase.results.map((result, index) => (
-          <li key={index}>{result}</li>
-        ))}
-      </ul>
-    </div>
+    </>
   );
 }
 
